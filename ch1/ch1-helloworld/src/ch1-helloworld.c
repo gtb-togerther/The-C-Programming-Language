4c5d6ba8aed11935745cf71c99e36d59f7f5da61
Dijack Dong <dijack.dong@gmail.com>
2012-09-10 16:47:15 +0800

    Add the example ch1-helloworld

---
+++ ch1/ch1-helloworld/src/ch1-helloworld.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : ch1-helloworld.c
+ Author      : Dijack Dong
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World"); /* prints Hello World */
+	return EXIT_SUCCESS;
+}
